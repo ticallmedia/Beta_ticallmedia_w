33d0dbb74d418fd408eac6dd6e3e2965938e33a9
Cesar Chavarriaga <support.ti@ticall-media.com>
2026-02-10 23:00:05 -0500

    Respuesta Agente Humano 21.93

--- app.py
+++ app.py
@@ -876,12 +876,12 @@ def request1_messages(ESTADO_USUARIO, telefono_id, lang):
         telefono_id, 
         message_response_for_list, 
         'list', 
-        list_titles = ["📱DDA & Mobile Campaigns","📊Display Media Planning","🛒Ecommerce Strategy",
+        list_titles = ["🔄TicAll Flow®️Ecosys","📊Display Media Planning","🛒Ecommerce Strategy",
                        "📣Paid Social Media","🎯Audience Studies","🚀Digital Marketing",
                        "📰Media Strategy","🤖Custom Bot Development","🌐WebSites",
                        "🗣️Talk to an Agent"], # El titulo no debe superar 24 caracteres
         list_ids = ["btn_1","btn_2","btn_3","btn_4","btn_5","btn_6","btn_7","btn_8","btn_9","btn_0"],           # Pasamos los IDs fijos
-        list_descrip=["Physical & Mobile Convert.",
+        list_descrip=["Growth Infrastructure Design",
                       "Formats and Creatives",
                       "Leading Strategies",
                       "Paid Social Media Mgmt",
